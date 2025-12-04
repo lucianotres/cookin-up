ffae089a65e6a7814d57ff2675508b1e865884bd
Luciano Tres <lucianotres@outlook.com>
2025-12-04 14:32:07 -0300

    Ajustado para ler a origem da URL da API do .env

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,14 +1,16 @@
 import type ICategorias from "@/interfaces/ICategorias";
 import type IReceita from "@/interfaces/IReceita";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export async function obterCategorias() {
-  const resposta = await fetch("https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json");
+  const resposta = await fetch(`${API_BASE_URL}categorias`);
   const categorias: ICategorias[] = await resposta.json();
   return categorias;
 }
 
 export async function obterReceitas() {
-  const resposta = await fetch("https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json");
+  const resposta = await fetch(`${API_BASE_URL}receitas`);
   const receitas: IReceita[] = await resposta.json();
   return receitas;
 }
