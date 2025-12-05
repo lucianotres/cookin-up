2de0d02177d1e951a0f982760e6815529a0655fb
Luciano Tres <lucianotres@outlook.com>
2025-12-04 22:47:05 -0300

    Correção do endereço do endpoint publico

--- src/http/index.ts
+++ src/http/index.ts
@@ -4,13 +4,13 @@ import type IReceita from "@/interfaces/IReceita";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function obterCategorias() {
-  const resposta = await fetch(`${API_BASE_URL}categorias`);
+  const resposta = await fetch(`${API_BASE_URL}publico/categorias`);
   const categorias: ICategorias[] = await resposta.json();
   return categorias;
 }
 
 export async function obterReceitas() {
-  const resposta = await fetch(`${API_BASE_URL}receitas`);
+  const resposta = await fetch(`${API_BASE_URL}publico/receitas`);
   const receitas: IReceita[] = await resposta.json();
   return receitas;
 }
