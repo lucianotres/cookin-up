958594b48dfc9083bd80d8e68cb71af360fb9e91
Luciano Tres <lucianotres@outlook.com>
2025-12-10 15:56:55 -0300

    Novo menu para oferecer login

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
 import Banner from './components/Banner.vue';
 import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 import Rodape from './components/Rodape.vue';
+import Topo from './components/Topo.vue';
 
 export default {
-  components: { Banner, ConteudoPrincipal, Rodape }
+  components: { Banner, ConteudoPrincipal, Rodape, Topo }
 }
 </script>
 
 <template>
+  <Topo />
+
   <Banner />
 
   <ConteudoPrincipal />
---
+++ src/components/Topo.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="menu">
+    <a>Login <FontAwesomeIcon :icon="faRightToBracket" /></a>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<style lang="css" scoped>
+  .menu {
+    display: flex;
+    flex-flow: column;
+    align-items: end;
+    justify-content: center;
+    height: 3rem;
+    background-color: rgba(0, 40, 0, .3);
+    color: white;
+  }
+  .menu a {
+    cursor: pointer;
+    user-select: none;
+  }
+  .menu a:last-child {
+    margin-right: 1em;
+  }
+</style>
